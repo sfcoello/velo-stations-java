f79c2c332fabfc85b8c406cb2f322565dd032003
Sofi <Sofi@MacBook-Pro-de-Sofia.local>
2024-08-11 20:27:18 -0300

    Log impl

--- src/main/java/com/made/sc/controller/ProductController.java
+++ src/main/java/com/made/sc/controller/ProductController.java
@@ -3,18 +3,23 @@ package com.made.sc.controller;
 import com.made.sc.dto.VeloStationsResponse;
 import com.made.sc.service.ProductService;
 import com.made.sc.utils.ResponseMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/velo")
 public class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     @Autowired private ProductService productService;
 
-    @GetMapping(value = "/stations", consumes = "application/json")
+    @GetMapping(value = "/stations")
     public ResponseEntity<VeloStationsResponse> getStations() {
+        log.info("Get all stations call from ");
         return ResponseEntity.ok(
                 ResponseMapper.map(
                         productService.getStations())
--- src/main/java/com/made/sc/service/ProductService.java
+++ src/main/java/com/made/sc/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.made.sc.service;
 
+import com.made.sc.controller.ProductController;
 import com.made.sc.dto.VeloDto;
 import com.made.sc.repository.VeloRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,8 @@ public class ProductService {
     @Autowired
     private VeloRepository veloRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
+
     public VeloDto getStations()  { //TODO modify db to find only obj network
         return findAllStations().get(0);
 
@@ -20,6 +25,7 @@ public class ProductService {
 
     private List<VeloDto> findAllStations() {
         try {
+            log.info("Calling mongo to find all stations");
             return veloRepository.findAll();
 
         } catch (Exception e) {
